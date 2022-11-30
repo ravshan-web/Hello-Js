4eaa3a2ebc8f86ffef7be8f0bd58af2e78eadd1a
Ravshan-web <savurboyevr@gmail.com>
2022-11-30 15:12:20 +0500

    Hello JS

---
+++ app.js
@@ -0,0 +1,11 @@
+alert(`Welcome to my page`);
+prompt(`How old are you?`);
+confirm(`Dou yo want to learning Javascript?`);
+
+alert(`Wow, so let's go!`);
+
+console.log('- Heh, Javascript');
+console.log(`- Hey`);
+console.log(`- What is the type of Not a Number?`);
+console.log(`- Number!`);
+console.log(`Oh, sh*t`);
